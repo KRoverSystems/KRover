4db0278e38178f33d0b4b6dca29745096f63e0bc
pansilup <pansilu@smu.edu.sg>
2024-06-15 13:47:19 +0800

    Adding setup instructions for kernel.

--- oasis/kernel/linux-hwe-5.3.18/dep.sh
+++ oasis/kernel/linux-hwe-5.3.18/dep.sh
@@ -1,5 +1,5 @@
 #compile the KVM module and deploy it 
-make -j`nproc` modules /home/neo/smu/oasis/kernel/linux-hwe-5.3.18/arch/x86/kvm/
+make -j`nproc` modules /<PATH-TO>/oasis/kernel/linux-hwe-5.3.18/arch/x86/kvm/
 sudo cp arch/x86/kvm/{kvm.ko,kvm-intel.ko} /lib/modules/5.3.18+/kernel/arch/x86/kvm/
 sync 
 sudo rmmod kvm-intel && sudo rmmod kvm
